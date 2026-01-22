d183eb429c8bd1315ea9c6f1f1f7ed5448287d61
Andrew Kuktenko <andriy.kuktenko@gmail.com>
2026-01-22 18:40:36 -0500

    Change context-cursor repository URL from SSH to HTTPS in setup script

--- scripts/setup-libs.sh
+++ scripts/setup-libs.sh
@@ -5,5 +5,5 @@ CONTEXT_CURSOR_DIR="$LIBS_DIR/context-cursor"
 if [ ! -d "$CONTEXT_CURSOR_DIR" ]; then
   echo "Cloning context-cursor..."
   mkdir -p "$LIBS_DIR"
-  git clone git@github.com:PavelLaptev/context-cursor.git "$CONTEXT_CURSOR_DIR"
+  git clone https://github.com/PavelLaptev/context-cursor.git "$CONTEXT_CURSOR_DIR"
 fi
